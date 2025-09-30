6364c135c5a5da31bdcdf6d5ba2f53a1fee30783
mttechgh1 <140465334+mttechgh1@users.noreply.github.com>
2025-09-30 18:34:07 -0400

    Update PCA9865_MODULE.py
    updated notes, corrected error

--- PCA9865_MODULE.py
+++ PCA9865_MODULE.py
@@ -1,6 +1,8 @@
 # install PCA9865 driver - sudo pip3 install adafruit-circuitpython-servokit
+# refrences  -"techspassion", "FREEDOM TECH" - on Youtube
+#In config, "I2C" needs to be set ON
 from adafruit_servokit import ServoKit
-froom time import sleep
+from time import sleep
 kit=ServoKit(channels=16)
 servo=14 # toal channels used
 #input command for angles
